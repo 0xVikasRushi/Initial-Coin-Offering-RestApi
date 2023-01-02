82c4eb7d103d3a1927143d727d7fe82304d08cac
0xvikasrushi <0xvikas@gmail.com>
2023-01-02 22:38:26 +0530

    db collection errors fixed

--- contract.js
+++ contract.js
@@ -1,13 +1,14 @@
+const mongoose = require("mongoose");
+const Contract = require("./models/contract.js");
 const connectDB = require("./db/connect.js");
-const Contract = require("./models/contract");
-const ContractJson = require("./contract.json");
-const Start = async () => {
-  try {
-    await connectDB();
-    await Contract.create(ContractJson);
-    console.log("successfully sent data to db");
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-Start();
+connectDB();
+const Contract1 = new Contract({
+  tokenName: "TEST1",
+  Symbol: "TXT1",
+  contractAddress: "0x8b65E3846BDecfD20b3a767A5C9C526B63dE6212",
+  company: "Gensis pvt limited",
+  deployedOn: "ethereum",
+  decimal: 18,
+});
+
+Contract1.save();
--- db/connect.js
+++ db/connect.js
@@ -3,10 +3,8 @@ require("dotenv").config;
 const MONGO_URL = process.env.MONGO_URL;
 const connectDB = () => {
   try {
-    mongoose.connect(MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    mongoose.set("strictQuery", false);
+    mongoose.connect(MONGO_URL, { useNewUrlParser: true });
   } catch (error) {
     console.log(error.message);
   }
