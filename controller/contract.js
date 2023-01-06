aaee9bbe5859bbc533fa7410fa3ab7258cf284b0
0xvikasrushi <0xvikas@gmail.com>
2023-01-06 17:52:27 +0530

    Search Sort Added

--- controller/contract.js
+++ controller/contract.js
@@ -2,21 +2,39 @@ const Contract = require("./../models/contract.js");
 const { db } = require("./../models/contract.js");
 
 const getAllContracts = async (req, res) => {
-  const { featured, company } = req.query;
-
-  const queryObj = {};
-  if (featured) {
-    queryObj.featured = featured === "true" ? true : false;
-  }
-  if (company) {
-    queryObj.company = company
-  }
-  
   try {
-    const contract = await Contract.find(query);
-    const nbhints = contract.length;
-    let message = nbhints === 0 ? "no results found" : contract;
-    res.status(201).json({ msg: message, nbhints });
+    // ? sort?=+name,createdAt
+    const queryObj = {};
+    const {
+      company,
+      tokenName,
+      Symbol,
+      contractAddress,
+      deployedOn,
+      decimal,
+      featured,
+      sort,
+    } = req.query;
+
+    if (company) queryObj.company = company;
+    if (tokenName) {
+      queryObj.tokenName = { $regex: tokenName, $options: "i" };
+    }
+    if (Symbol) queryObj.Symbol = Symbol;
+    if (contractAddress) queryObj.contractAddress = contractAddress;
+    if (decimal) queryObj.decimal = decimal;
+    if (deployedOn) queryObj.deployedOn = deployedOn;
+    if (featured) queryObj.featured = featured;
+
+    let apiData = Contract.find(queryObj);
+    if (sort) {
+      const sortFix = sort.replace(",", " ");
+      queryObj.sort = apiData.sort(sortFix);
+    }
+
+    const contract = await apiData;
+    let message = contract.length === 0 ? "no results found" : contract;
+    res.status(201).json({ msg: message, nbhints: contract.length });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
@@ -24,8 +42,39 @@ const getAllContracts = async (req, res) => {
 
 const getAllContractsTesting = async (req, res) => {
   try {
-    const contract = await Contract.find({ decimal: 18 });
-    res.status(200).json({ msg: contract });
+    const queryObj = {};
+
+    const {
+      company,
+      tokenName,
+      Symbol,
+      contractAddress,
+      deployedOn,
+      decimal,
+      featured,
+      sort,
+    } = req.query;
+
+    if (company) queryObj.company = company;
+    if (tokenName) {
+      queryObj.tokenName = { $regex: tokenName, $options: "i" };
+    }
+    if (Symbol) queryObj.Symbol = Symbol;
+    if (contractAddress) queryObj.contractAddress = contractAddress;
+    if (decimal) queryObj.decimal = decimal;
+    if (deployedOn) queryObj.deployedOn = deployedOn;
+    if (featured) queryObj.featured = featured;
+
+    let apiData = Contract.find(queryObj);
+    // sorting functions
+    if (sort) {
+      const sortFix = sort.replace(",", " ");
+      queryObj.sort = apiData.sort(sortFix);
+    }
+
+    const contract = await apiData;
+    let message = contract.length === 0 ? "no results found" : contract;
+    res.status(201).json({ msg: message, nbhints: contract.length });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
