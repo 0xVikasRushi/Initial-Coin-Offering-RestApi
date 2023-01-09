8f0ed0080d71d12b44a6db513821d929b4e05fe1
0xvikasrushi <0xvikas@gmail.com>
2023-01-10 00:32:40 +0530

    contract  query params added

--- blockchain/contractStats.js
+++ blockchain/contractStats.js
@@ -1,20 +1,23 @@
 const ethers = require("ethers");
-// const getAbi = require("./getAbi");
-const axios = require("axios");
+const getAbi = require("./getAbi");
 
 async function contractStats(contractAddress) {
+  const ContractObj = {};
+  ContractObj.contractAddress = contractAddress;
+  const abi = await getAbi(contractAddress);
   const provider = new ethers.providers.JsonRpcProvider(
-    process.env.ALCHEMY_RPC_URL
+    process.env.ALCHEMY_RPC_URL_1
   );
-  const apiKey = process.env.ETHERSCAN_API_KEY;
-  const url = `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${apiKey}`;
-  const response = await axios.get(url);
-  const abi = response.data.result;
-
+  // ContractObj.abi = abi;
   const Contract = new ethers.Contract(contractAddress, abi, provider);
-  const accountAddress = "0x560ba0013be40573bb6c6537e8626aa1b5b7a0cc";
-  const balance = await Contract.balanceOf(accountAddress);
-  console.log(balance.toNumber());
-}
 
-contractStats("0x3A4811C0619aBe8A51737c71f34D148b8ceB3335");
+  const name = await Contract.name();
+  if (name) ContractObj.name = name;
+
+  const symbol = await Contract.symbol();
+  if (symbol) ContractObj.symbol = symbol;
+
+  console.log(ContractObj);
+  return ContractObj;
+}
+module.exports = contractStats;
--- blockchain/getAbi.js
+++ blockchain/getAbi.js
@@ -1,7 +1,4 @@
 const axios = require("axios");
-
-// const address = "0x3A4811C0619aBe8A51737c71f34D148b8ceB3335";
-// getAbi(address);
 async function getAbi(contractAddress) {
   const apiKey = process.env.ETHERSCAN_API_KEY;
   const url = `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${apiKey}`;
@@ -16,5 +13,4 @@ async function getAbi(contractAddress) {
     );
   }
 }
-
 module.exports = getAbi;
